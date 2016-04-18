d5ae2998a29556ce69a9a356142f075c2a585269
thaildh <thaildhse61389@fpt.edu.vn>
2016-04-18 19:09:14 +0700

    Added files via upload

---
+++ DemoJenkin/src/DemoJenkin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author a
+ */
+public class DemoJenkin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=4,b=7;
+        CalNum num=new CalNum();
+        num.add(a, b);
+        DivideNum num1=new DivideNum();
+        num1.devide(a, b);
+    }
+    
+}
